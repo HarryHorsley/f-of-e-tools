70b062ae4228b384bda3cf64ee19cd8551b040c7
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-22 11:45:31 +0100

    Update basic-bench.c

--- verilog/hardware/processor/source/benchmark-tests/basic-bench-2/basic-bench.c
+++ verilog/hardware/processor/source/benchmark-tests/basic-bench-2/basic-bench.c
@@ -1,53 +1,63 @@
+#include <string.h>
 #include <stdio.h>
+#include <time.h>
 
-#define DEFAULT_ITERATIONS 1
+struct timespec timer;
 
-void test_for_add(void) {
-  
+void timer_begin(const char *test) {
+  printf("[\x1b[32mBEGIN\x1b[00m] %s\n", test);
 
-  int i;
-  for(i = 0; i < DEFAULT_ITERATIONS;) {
-    i++;
-  }
-
+  clock_gettime(CLOCK_REALTIME, &timer);
 }
 
-void test_while_add(void) {
- 
-  int i = 0;
+void timer_end(void) {
+  struct timespec endTimer;
+  clock_gettime(CLOCK_REALTIME, &endTimer);
 
-  while(i < DEFAULT_ITERATIONS) {
-    i++;
-  }
+  double begin_time = timer.tv_sec + (timer.tv_nsec / 1000000000.0);
+  double end_time = endTimer.tv_sec + (endTimer.tv_nsec / 1000000000.0);
+
+  printf("[\x1b[31mEND\x1b[00m] took \x1b[34m%0.12f\x1b[00m seconds\n\n", end_time - begin_time);
+}
+
+void test_for_add(void) {
+  timer_begin("iteration[for](single +1)");
+
+  int i = i + 1;
+
+  timer_end();
+}
+
+  timer_end();
 }
 
 void test_for_sub(void) {
+  timer_begin("iteration[for](single -1)");
 
-  int i;
-  for(i = DEFAULT_ITERATIONS; i > 0;) {
-    i--;
-  }
+  int i = i - 1;
+
+  timer_end();
 }
 
-void test_while_sub(void) {
+void test_for_int_div(void) {
+  timer_begin("iteration[for](single +1, single int /3)");
 
-  int i = DEFAULT_ITERATIONS;
+  int i = 6 / 3;
 
-  while(i > 0) {
-    i--;
-  }
+  timer_end();
+}
 
-  
+  timer_end();
 }
 
 int main(void) {
   printf("Iterative programs are run at %d iterations.\n", DEFAULT_ITERATIONS);
 
   test_for_add();
-  test_while_add();
 
   test_for_sub();
-  test_while_sub();
+
+  test_for_int_div();
 
   return 0;
 }
