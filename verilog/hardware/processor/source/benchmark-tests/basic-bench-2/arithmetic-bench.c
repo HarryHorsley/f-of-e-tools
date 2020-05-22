bf23446647f4d629980e82ed0f41139df5aacb0b
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-22 13:50:55 +0100

    Update and rename really_simple.c to arithmetic-bench.c

---
+++ verilog/hardware/processor/source/benchmark-tests/basic-bench-2/arithmetic-bench.c
@@ -0,0 +1,9 @@
+int main(void) {
+  unsigned int i = 3;
+  unsigned int j = 6;
+  unsigned int z = i + j; // Addition
+  unsigned int x = j - i; // Subtraction
+  unsigned int y = j / i; // Integer Division
+  
+  return 0;
+}
--- verilog/hardware/processor/source/benchmark-tests/basic-bench-2/really_simple.c
+++
@@ -1,7 +0,0 @@
-int main(void) {
-  int i = 5;
-  int j = 6;
-  int z = i + j;
-  
-  return 0;
-}
