a4f6e1ea0995f137cb2d772334383c1e5d196661
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-22 11:58:59 +0100

    Update basic-bench.c

--- verilog/hardware/processor/source/benchmark-tests/basic-bench-2/basic-bench.c
+++ verilog/hardware/processor/source/benchmark-tests/basic-bench-2/basic-bench.c
@@ -2,24 +2,6 @@
 #include <stdio.h>
 #include <time.h>
 
-struct timespec timer;
-
-void timer_begin(const char *test) {
-  printf("[\x1b[32mBEGIN\x1b[00m] %s\n", test);
-
-  clock_gettime(CLOCK_REALTIME, &timer);
-}
-
-void timer_end(void) {
-  struct timespec endTimer;
-  clock_gettime(CLOCK_REALTIME, &endTimer);
-
-  double begin_time = timer.tv_sec + (timer.tv_nsec / 1000000000.0);
-  double end_time = endTimer.tv_sec + (endTimer.tv_nsec / 1000000000.0);
-
-  printf("[\x1b[31mEND\x1b[00m] took \x1b[34m%0.12f\x1b[00m seconds\n\n", end_time - begin_time);
-}
-
 void test_for_add(void) {
   timer_begin("iteration[for](single +1)");
 
