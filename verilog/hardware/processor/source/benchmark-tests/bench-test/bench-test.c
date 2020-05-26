b4e11784b56bd721371ce88c15f313cd9fe23bc8
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-26 12:55:40 +0100

    Update bench-test.c

--- verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
+++ verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
@@ -5,27 +5,27 @@
 double PI;
 typedef double complex cplx;
  
-void _fft(cplx buf[], cplx out[], int n, int step)
-{
-	if (step < n) {
-		_fft(out, buf, n, step * 2);
-		_fft(out + step, buf + step, n, step * 2);
+// void _fft(cplx buf[], cplx out[], int n, int step)
+// {
+// 	if (step < n) {
+// 		_fft(out, buf, n, step * 2);
+// 		_fft(out + step, buf + step, n, step * 2);
  
-		for (int i = 0; i < n; i += 2 * step) {
-			cplx t = cexp(-I * PI * i / n) * out[i + step];
-			buf[i / 2]     = out[i] + t;
-			buf[(i + n)/2] = out[i] - t;
-		}
-	}
-}
+// 		for (int i = 0; i < n; i += 2 * step) {
+// 			cplx t = cexp(-I * PI * i / n) * out[i + step];
+// 			buf[i / 2]     = out[i] + t;
+// 			buf[(i + n)/2] = out[i] - t;
+// 		}
+// 	}
+// }
  
-void fft(cplx buf[], int n)
-{
-	cplx out[n];
-	for (int i = 0; i < n; i++) out[i] = buf[i];
+// void fft(cplx buf[], int n)
+// {
+// 	cplx out[n];
+// 	for (int i = 0; i < n; i++) out[i] = buf[i];
  
-	_fft(buf, out, n, 1);
-}
+// 	_fft(buf, out, n, 1);
+// }
 
 int main(void)
 {
