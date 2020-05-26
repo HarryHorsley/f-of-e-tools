ebc07e137f9c984f8924de9d4d9259ec3549475b
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-26 12:41:05 +0100

    Update bench-test.c

--- verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
+++ verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
@@ -1,5 +1,33 @@
-int
-main(void)
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+ 
+double PI;
+typedef double complex cplx;
+ 
+void _fft(cplx buf[], cplx out[], int n, int step)
+{
+	if (step < n) {
+		_fft(out, buf, n, step * 2);
+		_fft(out + step, buf + step, n, step * 2);
+ 
+		for (int i = 0; i < n; i += 2 * step) {
+			cplx t = cexp(-I * PI * i / n) * out[i + step];
+			buf[i / 2]     = out[i] + t;
+			buf[(i + n)/2] = out[i] - t;
+		}
+	}
+}
+ 
+void fft(cplx buf[], int n)
+{
+	cplx out[n];
+	for (int i = 0; i < n; i++) out[i] = buf[i];
+ 
+	_fft(buf, out, n, 1);
+}
+
+int main(void)
 {
 	/*
 	 *	Reading from the special address 0x2000 will cause sail to
@@ -9,33 +37,14 @@ main(void)
 	 */
 	volatile unsigned int *		debugLEDs = (unsigned int *)0x8004000;
 
-  unsigned int i = 3;
-  unsigned int j = 6;
-  unsigned int z;
-  
-	enum
-	{
-		kSpinDelay = 400000,
-	};
-
 	while(1)
 	{
 		*debugLEDs = 0xFF;
-    z = i + j; // Addition
-    z = j - i; // Subtraction
-    z = j / i; // Division
-		/*
-		 *	Spin
-		 */
-		for (int j = 0; j < kSpinDelay; j++);
+		
+    	PI = atan2(1, 1) * 4;
+	cplx buf[] = {1, 1, 1, 1, 0, 0, 0, 0};
+	fft(buf, 8);
     
-		*debugLEDs = 0x00;
-    z = 3 + 6;
-    z = 6 - 3;
-    z = 6 / 3; // Integer division
-		/*
-		 *	Spin
-		 */
-		for (int j = 0; j < kSpinDelay; j++);		
+		*debugLEDs = 0x00;		
 	}
 }
