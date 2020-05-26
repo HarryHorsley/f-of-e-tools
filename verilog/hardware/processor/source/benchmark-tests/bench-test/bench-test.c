0a5942ae4cd3ed7f8668f56080e54f1511ef4a68
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-26 13:18:28 +0100

    Update bench-test.c

--- verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
+++ verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
@@ -1,80 +1,118 @@
-// #include <stdio.h>
-// #include <math.h>
-// #include <complex.h>
-#include <stdio.h>
-#include <stdlib.h>
+// // #include <stdio.h>
+// // #include <math.h>
+// // #include <complex.h>
 
-typedef struct vector_ {
-    void** data;
-    int size;
-    int count;
-} vector;
-
-void vector_init(vector *v)
-{
-	v->data = NULL;
-	v->size = 0;
-	v->count = 0;
-}
-
-void vector_add(vector *v, int *e)
-{
-	if (v->size == 0) {
-		v->size = 10;
-		v->data = malloc(sizeof(void*) * v->size);
-		memset(v->data, '\0', sizeof(void) * v->size);
-	}
-}
-
-// double PI;
-// typedef double complex cplx;
+// // double PI;
+// // typedef double complex cplx;
  
-// void _fft(cplx buf[], cplx out[], int n, int step)
+// // void _fft(cplx buf[], cplx out[], int n, int step)
+// // {
+// // 	if (step < n) {
+// // 		_fft(out, buf, n, step * 2);
+// // 		_fft(out + step, buf + step, n, step * 2);
+ 
+// // 		for (int i = 0; i < n; i += 2 * step) {
+// // 			cplx t = cexp(-I * PI * i / n) * out[i + step];
+// // 			buf[i / 2]     = out[i] + t;
+// // 			buf[(i + n)/2] = out[i] - t;
+// // 		}
+// // 	}
+// // }
+ 
+// // void fft(cplx buf[], int n)
+// // {
+// // 	cplx out[n];
+// // 	for (int i = 0; i < n; i++) out[i] = buf[i];
+ 
+// // 	_fft(buf, out, n, 1);
+// // }
+
+// int main(void)
 // {
-// 	if (step < n) {
-// 		_fft(out, buf, n, step * 2);
-// 		_fft(out + step, buf + step, n, step * 2);
- 
-// 		for (int i = 0; i < n; i += 2 * step) {
-// 			cplx t = cexp(-I * PI * i / n) * out[i + step];
-// 			buf[i / 2]     = out[i] + t;
-// 			buf[(i + n)/2] = out[i] - t;
-// 		}
-// 	}
-// }
- 
-// void fft(cplx buf[], int n)
-// {
-// 	cplx out[n];
-// 	for (int i = 0; i < n; i++) out[i] = buf[i];
- 
-// 	_fft(buf, out, n, 1);
-// }
-
-int main(void)
-{
-	/*
-	 *	Reading from the special address 0x2000 will cause sail to
-	 *	set the value of 8 of the FPGA's pins to the byte written
-	 *	to the address. See the PCF file for how those 8 pins are
-	 *	mapped.
-	 */
-	volatile unsigned int *		debugLEDs = (unsigned int *)0x8004000;
+// 	/*
+// 	 *	Reading from the special address 0x2000 will cause sail to
+// 	 *	set the value of 8 of the FPGA's pins to the byte written
+// 	 *	to the address. See the PCF file for how those 8 pins are
+// 	 *	mapped.
+// 	 */
+// 	volatile unsigned int *		debugLEDs = (unsigned int *)0x8004000;
 
 	
-
-	*debugLEDs = 0xFF;
+// 	while (1) {
+		
+// 	*debugLEDs = 0xFF;
 		
 	
-	vector v;
-	vector_init(&v);
-	vector_add(&v, 0);
 		
-//     	PI = atan2(1, 1) * 4;
-// 	cplx buf[] = {1, 1, 0, 0};
-// 	_fft(buf, buf, 1, 1);
-// 	//fft(buf, 4);
+// //     	PI = atan2(1, 1) * 4;
+// // 	cplx buf[] = {1, 1, 0, 0};
+// // 	_fft(buf, buf, 1, 1);
+// // 	//fft(buf, 4);
     
-	*debugLEDs = 0x00;		
+// 	*debugLEDs = 0x00;
 	
+// 	}
+	
+// }
+
+#include <math.h>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+// timer cribbed from
+// https://gist.github.com/gongzhitaao/7062087
+class Timer
+{
+public:
+    Timer() : beg_(clock_::now()) {}
+    void reset() { beg_ = clock_::now(); }
+    double elapsed() const {
+        return duration_cast<second_>
+            (clock_::now() - beg_).count();
+    }
+
+private:
+    typedef high_resolution_clock clock_;
+    typedef duration<double, ratio<1>> second_;
+    time_point<clock_> beg_;
+};
+
+int main(char* argv)
+{
+    double total;
+    Timer tmr;
+
+#define randf() ((double) rand()) / ((double) (RAND_MAX))
+#define OP_TEST(name, expr)               \
+    total = 0.0;                          \
+    srand(42);                            \
+    tmr.reset();                          \
+    for (int i = 0; i < 100000000; i++) { \
+        double r1 = randf();              \
+        double r2 = randf();              \
+        total += expr;                    \
+    }                                     \
+    double name = tmr.elapsed();          \
+    printf(#name);                        \
+    printf(" %.7f\n", name - baseline);
+
+    // time the baseline code:
+    //   for loop with no extra math op
+    OP_TEST(baseline, 1.0)
+
+    // time various floating point operations.
+    //   subtracts off the baseline time to give
+    //   a better approximation of the cost
+    //   for just the specified operation
+    OP_TEST(plus, r1 + r2)
+    OP_TEST(minus, r1 - r2)
+    OP_TEST(mult, r1 * r2)
+    OP_TEST(div, r1 / r2)
+    OP_TEST(sqrt, sqrt(r1))
+    OP_TEST(sin, sin(r1))
+    OP_TEST(cos, cos(r1))
+    OP_TEST(tan, tan(r1))
+    OP_TEST(atan, atan(r1))
+    OP_TEST(exp, exp(r1))
 }
