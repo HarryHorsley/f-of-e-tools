7251eab215d8f999bf109178fb0f3172cf6fffe3
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-26 13:08:43 +0100

    Update bench-test.c

--- verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
+++ verilog/hardware/processor/source/benchmark-tests/bench-test/bench-test.c
@@ -1,23 +1,59 @@
+// #include <stdio.h>
+// #include <math.h>
+// #include <complex.h>
 #include <stdio.h>
-#include <math.h>
-#include <complex.h>
- 
-double PI;
-typedef double complex cplx;
- 
-void _fft(cplx buf[], cplx out[], int n, int step)
+#include <stdlib.h>
+
+#include "vector.h"
+
+typedef struct vector_ {
+    void** data;
+    int size;
+    int count;
+} vector;
+
+void vector_init(vector *v)
 {
-	if (step < n) {
-		_fft(out, buf, n, step * 2);
-		_fft(out + step, buf + step, n, step * 2);
- 
-		for (int i = 0; i < n; i += 2 * step) {
-			cplx t = cexp(-I * PI * i / n) * out[i + step];
-			buf[i / 2]     = out[i] + t;
-			buf[(i + n)/2] = out[i] - t;
-		}
-	}
+	v->data = NULL;
+	v->size = 0;
+	v->count = 0;
 }
+
+void vector_add(vector *v, void *e)
+{
+	if (v->size == 0) {
+		v->size = 10;
+		v->data = malloc(sizeof(void*) * v->size);
+		memset(v->data, '\0', sizeof(void) * v->size);
+	}
+
+	// condition to increase v->data:
+	// last slot exhausted
+	if (v->size == v->count) {
+		v->size *= 2;
+		v->data = realloc(v->data, sizeof(void*) * v->size);
+	}
+
+	v->data[v->count] = e;
+	v->count++;
+}
+
+// double PI;
+// typedef double complex cplx;
+ 
+// void _fft(cplx buf[], cplx out[], int n, int step)
+// {
+// 	if (step < n) {
+// 		_fft(out, buf, n, step * 2);
+// 		_fft(out + step, buf + step, n, step * 2);
+ 
+// 		for (int i = 0; i < n; i += 2 * step) {
+// 			cplx t = cexp(-I * PI * i / n) * out[i + step];
+// 			buf[i / 2]     = out[i] + t;
+// 			buf[(i + n)/2] = out[i] - t;
+// 		}
+// 	}
+// }
  
 // void fft(cplx buf[], int n)
 // {
@@ -41,10 +77,16 @@ int main(void)
 	{
 		*debugLEDs = 0xFF;
 		
-    	PI = atan2(1, 1) * 4;
-	cplx buf[] = {1, 1, 0, 0};
-	_fft(buf, buf, 1, 1);
-	//fft(buf, 4);
+	
+	vector v;
+	vector_init(&v);
+
+	vector_add(&v, 0123);
+		
+//     	PI = atan2(1, 1) * 4;
+// 	cplx buf[] = {1, 1, 0, 0};
+// 	_fft(buf, buf, 1, 1);
+// 	//fft(buf, 4);
     
 		*debugLEDs = 0x00;		
 	}
