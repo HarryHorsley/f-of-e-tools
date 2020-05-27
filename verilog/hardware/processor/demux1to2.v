8eea69f664d74f7a6a1f85f544ffbe05c9db9e51
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-27 18:42:34 +0100

    Update demux1to2.v

--- verilog/hardware/processor/demux1to2.v
+++ verilog/hardware/processor/demux1to2.v
@@ -1,1 +1,14 @@
-module 
+module demuxe1to2(
+  input [31:0] input0,
+  input select,
+  output [31:0] output0,
+  output [31:0] output1
+)
+  if (select)
+    assign output1 = input0;
+    assign output0 = 0; //just set to 0 here?
+  else
+    assign output0 = input0;
+    assign output1 = 0;
+  
+      
