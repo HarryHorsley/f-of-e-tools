e7ce1ef3774c8c768b06b327033ec240fad0c054
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-29 15:23:59 +0100

    Update v2_branch_predictor.v

--- verilog/hardware/processor/upgrades/v2_branch_predictor.v
+++ verilog/hardware/processor/upgrades/v2_branch_predictor.v
@@ -73,17 +73,7 @@ module branch_predictor(
 	reg [1:0]	s_reg[2**14-1:0];
 	assign 		pc_addr = in_addr >> 2;
 	reg		branch_mem_sig_reg;
-
-	/*
-	 *	The `initial` statement below uses Yosys's support for nonzero
-	 *	initial values:
-	 *
-	 *		https://github.com/YosysHQ/yosys/commit/0793f1b196df536975a044a4ce53025c81d00c7f
-	 *
-	 *	Rather than using this simulation construct (`initial`),
-	 *	the design should instead use a reset signal going to
-	 *	modules in the design and to thereby set the values.
-	 */
+	
 	initial begin
 		s_reg[2**14-1:0] = 2'b00;
 		branch_mem_sig_reg = 1'b0;
@@ -93,15 +83,10 @@ module branch_predictor(
 		branch_mem_sig_reg <= branch_mem_sig;
 	end
 
-	/*
-	 *	Using this microarchitecture, branches can't occur consecutively
-	 *	therefore can use branch_mem_sig as every branch is followed by
-	 *	a bubble, so a 0 to 1 transition
-	 */
 	always @(posedge clk) begin
 		if (branch_mem_sig_reg) begin
 			s_reg[pc_addr][1] <= (s_reg[pc_addr][1]&s_reg[pc_addr][0]) | (s_reg[pc_addr][0]&actual_branch_decision) | (s_reg[pc_addr][1]&actual_branch_decision);
-			//s[0] <= (s[1]&(!s[0])) | ((!s[0])&actual_branch_decision) | (s[1]&actual_branch_decision);
+			
 			s_reg[pc_addr][0] <= actual_branch_decision;
 		end
 	end
