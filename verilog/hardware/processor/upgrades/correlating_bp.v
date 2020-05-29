24b1b66111636d209e39a4b1186057e099f3d4a0
HarryHorsley <59326804+HarryHorsley@users.noreply.github.com>
2020-05-29 15:19:51 +0100

    Update correlating_bp.v

--- verilog/hardware/processor/upgrades/correlating_bp.v
+++ verilog/hardware/processor/upgrades/correlating_bp.v
@@ -73,17 +73,7 @@ module branch_predictor(
   reg [3:0] branch_history_reg;
   reg [2:0] global_history_reg [15:0];
 	reg		branch_mem_sig_reg;
-
-	/*
-	 *	The `initial` statement below uses Yosys's support for nonzero
-	 *	initial values:
-	 *
-	 *		https://github.com/YosysHQ/yosys/commit/0793f1b196df536975a044a4ce53025c81d00c7f
-	 *
-	 *	Rather than using this simulation construct (`initial`),
-	 *	the design should instead use a reset signal going to
-	 *	modules in the design and to thereby set the values.
-	 */
+	
 	initial begin
 		branch_history_reg = 4'b0000;
 		global_history_reg[15:0] = 2'b00;
@@ -93,12 +83,7 @@ module branch_predictor(
 	always @(negedge clk) begin
 		branch_mem_sig_reg <= branch_mem_sig;
 	end
-
-	/*
-	 *	Using this microarchitecture, branches can't occur consecutively
-	 *	therefore can use branch_mem_sig as every branch is followed by
-	 *	a bubble, so a 0 to 1 transition
-	 */
+	
 	always @(posedge clk) begin
 		if (branch_mem_sig_reg) begin
 			branch_history_reg <= branch_history_reg << 1;
